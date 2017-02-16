a200d91ce82b811a852f0ba7672dc92d0893ac18
baku89 <mail@baku89.com>
2017-02-17 06:12:56 +0900

    Refactor SctendedFBXManager::drawPart()

--- src/ExtendedFBXManager.h
+++ src/ExtendedFBXManager.h
@@ -8,12 +8,39 @@
 class ExtendedFBXManager : public ofxFBXManager {
 public:
 	
-	void drawPart(int type) {
+	void setup(ofxFBXScene* aScene) {
+		ofxFBXManager::setup(aScene);
+		
+		screenMeshIndices.clear();
+		stageMeshIndices.clear();
+		guideMeshIndices.clear();
+		
 		vector< shared_ptr<ofxFBXMesh> > fbxMeshes = fbxScene->getMeshes();
 		for(int i = 0; i < fbxMeshes.size(); i++ ) {
 			
+			switch (getNodeType(fbxMeshes[i])) {
+				case NODE_TYPE_SCREEN:	screenMeshIndices.push_back(i); break;
+				case NODE_TYPE_STAGE:	stageMeshIndices.push_back(i); break;
+				case NODE_TYPE_GUIDE:	guideMeshIndices.push_back(i); break;
+			}
+		}
+	}
+	
+	void drawPart(int type) {
+		
+		vector< shared_ptr<ofxFBXMesh> > fbxMeshes = fbxScene->getMeshes();
+		
+		vector<int> *meshIndices;
+		
+		switch (type) {
+			case NODE_TYPE_SCREEN:	meshIndices = &screenMeshIndices; break;
+			case NODE_TYPE_STAGE:	meshIndices = &stageMeshIndices; break;
+			case NODE_TYPE_GUIDE:	meshIndices = &guideMeshIndices; break;
+		}
+		
+		for(int i : *meshIndices) {
+			
 			if ( type == getNodeType(fbxMeshes[i]) ) {
-				
 				meshTransforms[i].transformGL();
 				fbxMeshes[i]->draw( &meshes[i] );
 				meshTransforms[i].restoreTransformGL();
@@ -33,6 +60,8 @@ private:
 			return NODE_TYPE_STAGE;
 		}
 	}
+	
+	vector<int> screenMeshIndices, stageMeshIndices, guideMeshIndices;
 	
 	const string screenPrefix = "screen";
 	const string guidePrefix = "guide";
