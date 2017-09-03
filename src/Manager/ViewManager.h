013ac6c8e2c9a771a8037457aed9f1ab44e1f754
Baku Hashimoto <mail@baku89.com>
2017-09-03 18:02:56 +0900

    Refactor misc codes

--- src/Manager/ViewManager.h
+++ src/Manager/ViewManager.h
@@ -10,6 +10,10 @@
 
 #define CAM_INDEX_CUSTOM    0
 
+
+inline void createCamera(ofMesh *mesh);
+
+
 class ViewManager : public BaseManager {
 public:
 	
@@ -22,57 +26,7 @@ public:
 		visibility["guides"].set("Guides", true);
 		visibility["cameras"].set("Cameras", true);
 		
-		{
-			// create camera
-			
-			vector<ofVec3f> vs;
-			
-			vs.push_back(ofVec3f( 12.5, 12.5, 12.5));
-			vs.push_back(ofVec3f( 12.5, 12.5, 62.5));
-			vs.push_back(ofVec3f(-12.5, 12.5, 12.5));
-			vs.push_back(ofVec3f(-12.5, 12.5, 62.5));
-			
-			vs.push_back(ofVec3f( 12.5, -12.5, 12.5));
-			vs.push_back(ofVec3f( 12.5, -12.5, 62.5));
-			vs.push_back(ofVec3f(-12.5, -12.5, 12.5));
-			vs.push_back(ofVec3f(-12.5, -12.5, 62.5));
-			
-			vs.push_back(vs[0]); vs.push_back(vs[2]);
-			vs.push_back(vs[1]); vs.push_back(vs[3]);
-			vs.push_back(vs[4]); vs.push_back(vs[6]);
-			vs.push_back(vs[5]); vs.push_back(vs[7]);
-			
-			vs.push_back(vs[0]); vs.push_back(vs[4]);
-			vs.push_back(vs[1]); vs.push_back(vs[5]);
-			vs.push_back(vs[2]); vs.push_back(vs[6]);
-			vs.push_back(vs[3]); vs.push_back(vs[7]);
-			cameraMesh.addVertices(vs);
-			
-			vs.clear();
-			
-			vs.push_back(ofVec3f(   11,   11,    0));
-			vs.push_back(ofVec3f( 3.75, 3.75, 12.5));
-			vs.push_back(ofVec3f(  -11,   11,    0));
-			vs.push_back(ofVec3f(-3.75, 3.75, 12.5));
-			
-			vs.push_back(ofVec3f(   11,  -11,    0));
-			vs.push_back(ofVec3f( 3.75,-3.75, 12.5));
-			vs.push_back(ofVec3f(  -11,  -11,    0));
-			vs.push_back(ofVec3f(-3.75,-3.75, 12.5));
-			
-			vs.push_back(vs[0]); vs.push_back(vs[2]);
-			vs.push_back(vs[1]); vs.push_back(vs[3]);
-			vs.push_back(vs[4]); vs.push_back(vs[6]);
-			vs.push_back(vs[5]); vs.push_back(vs[7]);
-			
-			vs.push_back(vs[0]); vs.push_back(vs[4]);
-			vs.push_back(vs[1]); vs.push_back(vs[5]);
-			vs.push_back(vs[2]); vs.push_back(vs[6]);
-			vs.push_back(vs[3]); vs.push_back(vs[7]);
-			
-			cameraMesh.addVertices(vs);
-			cameraMesh.setMode(OF_PRIMITIVE_LINES);
-		}
+        createCamera(&cameraMesh);
 		
 		font.load("Karla-Regular.ttf", 9);
 		font.setLetterSpacing(1.1);
@@ -555,3 +509,54 @@ private:
 	
 	bool				enableScreenBlending;
 };
+
+inline void createCamera(ofMesh *mesh) {
+    // create camera
+    vector<ofVec3f> vs;
+    
+    vs.push_back(ofVec3f( 12.5, 12.5, 12.5));
+    vs.push_back(ofVec3f( 12.5, 12.5, 62.5));
+    vs.push_back(ofVec3f(-12.5, 12.5, 12.5));
+    vs.push_back(ofVec3f(-12.5, 12.5, 62.5));
+    
+    vs.push_back(ofVec3f( 12.5, -12.5, 12.5));
+    vs.push_back(ofVec3f( 12.5, -12.5, 62.5));
+    vs.push_back(ofVec3f(-12.5, -12.5, 12.5));
+    vs.push_back(ofVec3f(-12.5, -12.5, 62.5));
+    
+    vs.push_back(vs[0]); vs.push_back(vs[2]);
+    vs.push_back(vs[1]); vs.push_back(vs[3]);
+    vs.push_back(vs[4]); vs.push_back(vs[6]);
+    vs.push_back(vs[5]); vs.push_back(vs[7]);
+    
+    vs.push_back(vs[0]); vs.push_back(vs[4]);
+    vs.push_back(vs[1]); vs.push_back(vs[5]);
+    vs.push_back(vs[2]); vs.push_back(vs[6]);
+    vs.push_back(vs[3]); vs.push_back(vs[7]);
+    mesh->addVertices(vs);
+    
+    vs.clear();
+    
+    vs.push_back(ofVec3f(   11,   11,    0));
+    vs.push_back(ofVec3f( 3.75, 3.75, 12.5));
+    vs.push_back(ofVec3f(  -11,   11,    0));
+    vs.push_back(ofVec3f(-3.75, 3.75, 12.5));
+    
+    vs.push_back(ofVec3f(   11,  -11,    0));
+    vs.push_back(ofVec3f( 3.75,-3.75, 12.5));
+    vs.push_back(ofVec3f(  -11,  -11,    0));
+    vs.push_back(ofVec3f(-3.75,-3.75, 12.5));
+    
+    vs.push_back(vs[0]); vs.push_back(vs[2]);
+    vs.push_back(vs[1]); vs.push_back(vs[3]);
+    vs.push_back(vs[4]); vs.push_back(vs[6]);
+    vs.push_back(vs[5]); vs.push_back(vs[7]);
+    
+    vs.push_back(vs[0]); vs.push_back(vs[4]);
+    vs.push_back(vs[1]); vs.push_back(vs[5]);
+    vs.push_back(vs[2]); vs.push_back(vs[6]);
+    vs.push_back(vs[3]); vs.push_back(vs[7]);
+    
+    mesh->addVertices(vs);
+    mesh->setMode(OF_PRIMITIVE_LINES);
+}
